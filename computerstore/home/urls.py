1aae1b9cb755a3f56b9fe57648ac5a1153fc74cb
AllanZhantaykozha <103952115+AllanZhantaykozha@users.noreply.github.com>
2022-08-11 19:07:45 +0600

    ---

--- computerstore/home/urls.py
+++ computerstore/home/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import HomePage
+from .views import HomePage, ComputerPage, AccessoryPage
 
 
 urlpatterns = [
-    path('', HomePage.as_view(), name='home')
+    path('', HomePage.as_view(), name='home'),
+    path('computers/', ComputerPage.as_view(), name='computer'),
+    path('accessories/', AccessoryPage.as_view(), name='accessory'),
+#    path('contact/', ContactPage.as_view(), name='contact'),
 ]
--- computerstore/home/views.py
+++ computerstore/home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from .models import PC, Accessory, Type, News
 
+#90887
 
 class HomePage(ListView):
     model = News
@@ -11,4 +12,24 @@ class HomePage(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Home'
+        return context
+
+class ComputerPage(ListView):
+    model = PC
+    template_name = 'home/computerpage.html'
+    context_object_name = 'pc'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Computer Shop'
+        return context
+
+class AccessoryPage(ListView):
+    model = Accessory
+    template_name = 'home/accessorypage.html'
+    context_object_name = 'acc'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Accessory Shop'
         return context
